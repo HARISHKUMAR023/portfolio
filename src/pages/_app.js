bb02b998fb45d4953d037bcd13ee684d8844117f
HARISHKUMAR N <harishkumarsp023@gmail.com>
2024-08-08 08:32:39 +0530

    add google anglitics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,10 +3,12 @@ import Transition from '../components/Transition';
 import '../app/globals.css';
 import { useRouter} from 'next/router';
 import { AnimatePresence , motion } from 'framer-motion';
+import { GoogleAnalytics } from '@next/third-parties/google'
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <Layout>
+            <GoogleAnalytics gaId="G-LYNWEP9V8T" />
       <AnimatePresence mode='wait'>
         <motion.div key={router.route} className='h-full'>
           <Transition/>
