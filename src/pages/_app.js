82608c204af59e529f09a6b7e8f792d218e2ec3d
HARISHKUMAR N <harishkumarsp023@gmail.com>
2024-08-08 08:50:03 +0530

    add googleAnalytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,12 @@ import Transition from '../components/Transition';
 import '../app/globals.css';
 import { useRouter} from 'next/router';
 import { AnimatePresence , motion } from 'framer-motion';
-import { GoogleAnalytics } from '@next/third-parties/google'
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <Layout>
-            <GoogleAnalytics gaId="G-LYNWEP9V8T" />
+      <GoogleAnalytics />
       <AnimatePresence mode='wait'>
         <motion.div key={router.route} className='h-full'>
           <Transition/>
